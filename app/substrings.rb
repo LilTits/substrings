b38ed69f7a459f0e79922577e54f784822783055
Titouan <titouan@guiochet.com>
2020-10-05 07:05:15 +0200

    Initial commit

---
+++ app/substrings.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Implementation of substrings
+class Substrings
+  'hello world'
+end
---
+++ spec/substrings_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+require_relative '../app/substrings'
+
+RSpec.describe Substrings do
+  it 'should be true' do
+    expect(true).to be true
+  end
+end
